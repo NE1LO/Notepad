7aecc13dc7c487d5e2eff666b1277046ec78c014
Anton S <asmeiko@gmail.com>
2025-01-19 23:56:37 +0100

    add tailwind

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-blue-500 text-white text-center p-6">
+    Tailwind CSS работает!
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+  
+</style>
