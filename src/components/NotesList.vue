83c78db2dfd2e00a156dc2b9a531affc947ed508
Anton S <asmeiko@gmail.com>
2025-01-20 23:08:13 +0100

    Update NotesList.vue

--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -3,13 +3,12 @@
     <div class="mt-8">
         <div class="flex justify-center gap-4 h-10">
             <input
-                v-model="editedText"
+                v-model="newTask"
                 class="p-2 w-1/2"
                 placeholder="Text of task"
          />
-         <button class="bg-blue-500 h-full">Save</button>
+         <button @click="addTask" class="bg-blue-500 h-full">Save</button>
         </div>
-        
     </div>
 
 </template>
