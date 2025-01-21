bfbe84a373c88bfdaa4cf7f587961a50be8dfe11
Anton S <asmeiko13@gmail.com>
2025-01-21 23:31:46 +0100

    Update component TaskList

---
+++ src/components/TasksList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <CreateTask />
+  </div>
+</template>
+
+<script>
+import CreateTask from "./CreateTask.vue";
+
+export default {
+  components: {
+    CreateTask,
+  },
+
+};
+</script>
+
+<style></style>
