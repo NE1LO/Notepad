4e8f249e59059d48f4b6662e32ef3406b9c5bb79
Anton S <asmeiko13@gmail.com>
2025-01-21 23:32:25 +0100

    Create component TaskItem

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <ul>
+        <li> <p>text</p> </li>
+        <li> <button>Edit</button> </li>
+        <li> <button>Delete</button> </li>
+        <li> <button> </button> </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
