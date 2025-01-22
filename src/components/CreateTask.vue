361c93529a4f65d278ddb0ff9e728bac314a8ecb
Anton S <asmeiko13@gmail.com>
2025-01-22 23:04:46 +0100

    Update CreateTask.vue

--- src/components/CreateTask.vue
+++ src/components/CreateTask.vue
@@ -2,27 +2,21 @@
   <div class="mt-8">
     <div class="flex justify-center gap-4 h-10">
       <input v-model="newTask" class="p-2 w-1/2" placeholder="Text of task" />
-      <button @click="addTask" class="bg-blue-500 h-full">Save</button>
+      <button @click="handleAddTask" class="bg-blue-500 h-full">Save</button>
     </div>
-    <ul>
-      <li v-for="(task, index) in tasks" :key="index" class="">
-        <p>{{ task }}</p>
-      </li>
-    </ul>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      tasks: [],
       newTask: "",
     };
   },
   methods: {
-    addTask() {
+    handleAddTask() {
       if (this.newTask.trim()) {
-        this.tasks.push(this.newTask.trim());
+        this.$emit("add-task", this.newTask.trim());
         this.newTask = "";
       }
     },
